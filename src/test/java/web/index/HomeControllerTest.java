383c3654d04f8c7cc11d255ba414a00c7aa2fc52
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-08-28 13:44:10 +0900

    Initial Setting and welcomePage(+test) updated.

---
+++ src/test/java/web/index/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package web.index;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)//컨트롤러 슬라이싱 테스트
+
+public class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void test_indexRoot()throws Exception{
+
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
